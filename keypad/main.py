15927ac46578b4268f11f872cdc8dd02163ba330
Antek24Git <antek.j.m@protonmail.com>
2024-03-22 17:01:56 -0600

    All com support
    I added a input requesting the user for their com port and stopped the exe from instantly closing on startup.

--- keypad/main.py
+++ keypad/main.py
@@ -2,9 +2,11 @@
 import serial
 import time
 import pyautogui
-# Assign port variable to port used by Arduino board
 
-port = "com4"
+# Assign port variable to port used by Arduino board
+com_num = input("hi, please select your com port number(numbers 1-5)\a \n")
+port = "com" + com_num
+
 
 # Connect to the Arduino using the serial library
 
@@ -18,16 +20,18 @@ time.sleep(1)
 while True:
 
     # Wait while there is no data
-    while (arduino_data.inWaiting() == 0):
+    while arduino_data.inWaiting() == 0:
         pass
 
     # Read data when avaliable
     data_packet = arduino_data.readline()
 
     # Convert from byte to string
-    data_packet = str(data_packet, 'utf-8')
+    data_packet = str(data_packet, "utf-8")
 
     # Remove new lines
-    data_packet = data_packet.strip('\r\n')
+    data_packet = data_packet.strip("\r\n")
 
     pyautogui.hotkey(f"{data_packet}")
+
+input("this is just so if you package the code to an exe it wont close :)")
